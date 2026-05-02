013fc689d5a64883c5bc8246813a9eb0d6658c4b
maddychetan <maddychetan@gmail.com>
2026-05-02 15:28:38 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+﻿import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/3d-resume/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  },
+  server: {
+    port: 3000
+  }
+})
